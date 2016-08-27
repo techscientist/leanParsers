11062971cc673f110b41505350b6d2aad1588d2c
bxute <ankit.kumar071460@gmail.com>
2016-08-27 17:50:06 +0530

    new feature added new msg added to tracing facec adding new classes

--- Parsers/CopyrightModel.java
+++ Parsers/CopyrightModel.java
@@ -1,21 +1,21 @@
 
 class CopyrightModel {
 
-	public String licence;
 	public String licence_url;
 	public String logo;
-	public String holder;
-	public int year;
+	public String licence;
 	public String holder_url;
+	public int year;
+	public String holder;
 
-	public CopyrightModel(String licence, String licence_url, String logo, String holder, int year, String holder_url) {
+	public CopyrightModel(String licence_url, String logo, String licence, String holder_url, int year, String holder) {
 
-		this.licence = licence;
 		this.licence_url = licence_url;
 		this.logo = logo;
-		this.holder = holder;
-		this.year = year;
+		this.licence = licence;
 		this.holder_url = holder_url;
+		this.year = year;
+		this.holder = holder;
 
 	}
 
--- Parsers/RootModel.java
+++ Parsers/RootModel.java
@@ -2,55 +2,55 @@ import java.util.ArrayList;
 
 class RootModel {
 
-	public String topic;
-	public String logo;
-	public VersionModel _version;
-	public Call_for_papersModel _call_for_papers;
-	public String email;
-	public String organizer_description;
-	public String privacy;
-	public int id;
-	public String type;
-	public ArrayList<Social_linksModel> social_links;
 	public String organizer_name;
-	public String background_image;
-	public String state;
-	public String name;
-	public String start_time;
 	public String code_of_conduct;
-	public String timezone;
-	public String description;
-	public CopyrightModel _copyright;
+	public String organizer_description;
 	public String schedule_published_on;
+	public String background_image;
+	public String privacy;
+	public Call_for_papersModel _call_for_papers;
 	public CreatorModel _creator;
-	public String location_name;
+	public String state;
+	public String email;
+	public String start_time;
 	public String end_time;
+	public ArrayList<Social_linksModel> social_links;
+	public String name;
+	public String timezone;
+	public String logo;
+	public CopyrightModel _copyright;
+	public String topic;
+	public String description;
+	public VersionModel _version;
+	public String type;
+	public int id;
+	public String location_name;
 
-	public RootModel(String topic, String logo, VersionModel version, Call_for_papersModel call_for_papers, String email, String organizer_description, String privacy, int id, String type, ArrayList<Social_linksModel> social_links, String organizer_name, String background_image, String state, String name, String start_time, String code_of_conduct, String timezone, String description, CopyrightModel copyright, String schedule_published_on, CreatorModel creator, String location_name, String end_time) {
+	public RootModel(String organizer_name, String code_of_conduct, String organizer_description, String schedule_published_on, String background_image, String privacy, Call_for_papersModel call_for_papers, CreatorModel creator, String state, String email, String start_time, String end_time, ArrayList<Social_linksModel> social_links, String name, String timezone, String logo, CopyrightModel copyright, String topic, String description, VersionModel version, String type, int id, String location_name) {
 
-		this.topic = topic;
-		this.logo = logo;
-		this._version = version;
-		this._call_for_papers = call_for_papers;
-		this.email = email;
-		this.organizer_description = organizer_description;
-		this.privacy = privacy;
-		this.id = id;
-		this.type = type;
-		this.social_links = social_links;
 		this.organizer_name = organizer_name;
-		this.background_image = background_image;
-		this.state = state;
-		this.name = name;
-		this.start_time = start_time;
 		this.code_of_conduct = code_of_conduct;
-		this.timezone = timezone;
-		this.description = description;
-		this._copyright = copyright;
+		this.organizer_description = organizer_description;
 		this.schedule_published_on = schedule_published_on;
+		this.background_image = background_image;
+		this.privacy = privacy;
+		this._call_for_papers = call_for_papers;
 		this._creator = creator;
-		this.location_name = location_name;
+		this.state = state;
+		this.email = email;
+		this.start_time = start_time;
 		this.end_time = end_time;
+		this.social_links = social_links;
+		this.name = name;
+		this.timezone = timezone;
+		this.logo = logo;
+		this._copyright = copyright;
+		this.topic = topic;
+		this.description = description;
+		this._version = version;
+		this.type = type;
+		this.id = id;
+		this.location_name = location_name;
 
 	}
 
