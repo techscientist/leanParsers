70d6a7928b412eb61f75bef17bbc8a64af1ff07d
bxute <ankit.kumar071460@gmail.com>
2016-08-27 17:49:17 +0530

    new feature added new msg added to tracing facec adding new classes

--- generate.sh
+++ generate.sh
@@ -7,7 +7,7 @@ for i in `seq 1 100`
 do
 file_=${files[$RANDOM % ${#files[@]} ]}
 location=$folder_name'/'$file_
-python3.5 generator.py $location
+python generator.py $location
 message=${messages[$RANDOM % ${#messages[@]} ]}
 git add .
 git commit -m "$message"
