2b4c8199b828dfe5c741577463bec7a954a922a0
jiowifi <xutephone16@gmail.com>
2016-08-27 20:29:43 +0530

    changes made are availble now with more choices

--- app/src/main/java/com/example/ankit/lean/Parsers/RootModel.java
+++ app/src/main/java/com/example/ankit/lean/Parsers/RootModel.java
@@ -2,55 +2,55 @@ import java.util.ArrayList;
 
 class RootModel {
 
-	public String end_time;
-	public VersionModel _version;
-	public String organizer_description;
-	public String background_image;
-	public String description;
-	public String email;
-	public CreatorModel _creator;
-	public String timezone;
-	public String privacy;
-	public CopyrightModel _copyright;
-	public String location_name;
-	public String state;
-	public String name;
-	public String code_of_conduct;
-	public String start_time;
-	public String organizer_name;
-	public String schedule_published_on;
-	public Call_for_papersModel _call_for_papers;
 	public String logo;
-	public ArrayList<Social_linksModel> social_links;
-	public String type;
 	public String topic;
+	public Call_for_papersModel _call_for_papers;
+	public String description;
+	public String timezone;
+	public String background_image;
+	public String state;
+	public String type;
+	public String schedule_published_on;
+	public String location_name;
+	public String end_time;
+	public String organizer_description;
+	public String name;
+	public String organizer_name;
+	public CreatorModel _creator;
+	public String code_of_conduct;
+	public ArrayList<Social_linksModel> social_links;
+	public String privacy;
+	public String start_time;
+	public String email;
 	public int id;
+	public CopyrightModel _copyright;
+	public VersionModel _version;
 
-	public RootModel(String end_time, VersionModel version, String organizer_description, String background_image, String description, String email, CreatorModel creator, String timezone, String privacy, CopyrightModel copyright, String location_name, String state, String name, String code_of_conduct, String start_time, String organizer_name, String schedule_published_on, Call_for_papersModel call_for_papers, String logo, ArrayList<Social_linksModel> social_links, String type, String topic, int id) {
+	public RootModel(String logo, String topic, Call_for_papersModel call_for_papers, String description, String timezone, String background_image, String state, String type, String schedule_published_on, String location_name, String end_time, String organizer_description, String name, String organizer_name, CreatorModel creator, String code_of_conduct, ArrayList<Social_linksModel> social_links, String privacy, String start_time, String email, int id, CopyrightModel copyright, VersionModel version) {
 
-		this.end_time = end_time;
-		this._version = version;
-		this.organizer_description = organizer_description;
-		this.background_image = background_image;
-		this.description = description;
-		this.email = email;
-		this._creator = creator;
-		this.timezone = timezone;
-		this.privacy = privacy;
-		this._copyright = copyright;
-		this.location_name = location_name;
-		this.state = state;
-		this.name = name;
-		this.code_of_conduct = code_of_conduct;
-		this.start_time = start_time;
-		this.organizer_name = organizer_name;
-		this.schedule_published_on = schedule_published_on;
-		this._call_for_papers = call_for_papers;
 		this.logo = logo;
-		this.social_links = social_links;
-		this.type = type;
 		this.topic = topic;
+		this._call_for_papers = call_for_papers;
+		this.description = description;
+		this.timezone = timezone;
+		this.background_image = background_image;
+		this.state = state;
+		this.type = type;
+		this.schedule_published_on = schedule_published_on;
+		this.location_name = location_name;
+		this.end_time = end_time;
+		this.organizer_description = organizer_description;
+		this.name = name;
+		this.organizer_name = organizer_name;
+		this._creator = creator;
+		this.code_of_conduct = code_of_conduct;
+		this.social_links = social_links;
+		this.privacy = privacy;
+		this.start_time = start_time;
+		this.email = email;
 		this.id = id;
+		this._copyright = copyright;
+		this._version = version;
 
 	}
 
