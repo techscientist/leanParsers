a60f8ccaaeb38279d908439413aa75619d402d7a
bxute <ankit.kumar071460@gmail.com>
2016-08-27 17:49:43 +0530

    new feature added new msg added to tracing facec adding new classes

--- Parsers/RootModel.java
+++ Parsers/RootModel.java
@@ -2,55 +2,55 @@ import java.util.ArrayList;
 
 class RootModel {
 
+	public Call_for_papersModel _call_for_papers;
+	public int id;
+	public String location_name;
+	public String description;
+	public String name;
+	public String timezone;
+	public String state;
+	public CopyrightModel _copyright;
 	public String background_image;
 	public ArrayList<Social_linksModel> social_links;
-	public String description;
-	public String location_name;
+	public String type;
+	public VersionModel _version;
+	public String end_time;
+	public String organizer_name;
+	public String code_of_conduct;
+	public String schedule_published_on;
+	public String privacy;
+	public String start_time;
+	public CreatorModel _creator;
 	public String organizer_description;
 	public String topic;
-	public String start_time;
-	public String code_of_conduct;
-	public String timezone;
 	public String email;
 	public String logo;
-	public Call_for_papersModel _call_for_papers;
-	public String organizer_name;
-	public String privacy;
-	public String state;
-	public String name;
-	public VersionModel _version;
-	public CreatorModel _creator;
-	public String type;
-	public CopyrightModel _copyright;
-	public String schedule_published_on;
-	public String end_time;
-	public int id;
 
-	public RootModel(String background_image, ArrayList<Social_linksModel> social_links, String description, String location_name, String organizer_description, String topic, String start_time, String code_of_conduct, String timezone, String email, String logo, Call_for_papersModel call_for_papers, String organizer_name, String privacy, String state, String name, VersionModel version, CreatorModel creator, String type, CopyrightModel copyright, String schedule_published_on, String end_time, int id) {
+	public RootModel(Call_for_papersModel call_for_papers, int id, String location_name, String description, String name, String timezone, String state, CopyrightModel copyright, String background_image, ArrayList<Social_linksModel> social_links, String type, VersionModel version, String end_time, String organizer_name, String code_of_conduct, String schedule_published_on, String privacy, String start_time, CreatorModel creator, String organizer_description, String topic, String email, String logo) {
 
+		this._call_for_papers = call_for_papers;
+		this.id = id;
+		this.location_name = location_name;
+		this.description = description;
+		this.name = name;
+		this.timezone = timezone;
+		this.state = state;
+		this._copyright = copyright;
 		this.background_image = background_image;
 		this.social_links = social_links;
-		this.description = description;
-		this.location_name = location_name;
+		this.type = type;
+		this._version = version;
+		this.end_time = end_time;
+		this.organizer_name = organizer_name;
+		this.code_of_conduct = code_of_conduct;
+		this.schedule_published_on = schedule_published_on;
+		this.privacy = privacy;
+		this.start_time = start_time;
+		this._creator = creator;
 		this.organizer_description = organizer_description;
 		this.topic = topic;
-		this.start_time = start_time;
-		this.code_of_conduct = code_of_conduct;
-		this.timezone = timezone;
 		this.email = email;
 		this.logo = logo;
-		this._call_for_papers = call_for_papers;
-		this.organizer_name = organizer_name;
-		this.privacy = privacy;
-		this.state = state;
-		this.name = name;
-		this._version = version;
-		this._creator = creator;
-		this.type = type;
-		this._copyright = copyright;
-		this.schedule_published_on = schedule_published_on;
-		this.end_time = end_time;
-		this.id = id;
 
 	}
 
