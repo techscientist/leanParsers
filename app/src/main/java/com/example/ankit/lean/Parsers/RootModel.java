36fafdbb35dc56336299f7393485e99343d54fb3
jiowifi <xutephone16@gmail.com>
2016-08-27 20:32:09 +0530

    changes made are availble now with more choices

--- app/src/main/java/com/example/ankit/lean/Parsers/RootModel.java
+++ app/src/main/java/com/example/ankit/lean/Parsers/RootModel.java
@@ -2,55 +2,55 @@ import java.util.ArrayList;
 
 class RootModel {
 
-	public String start_time;
-	public Call_for_papersModel _call_for_papers;
 	public String timezone;
-	public String logo;
-	public String location_name;
-	public ArrayList<Social_linksModel> social_links;
-	public String organizer_description;
-	public String description;
-	public CopyrightModel _copyright;
-	public String schedule_published_on;
 	public String type;
-	public String state;
-	public String background_image;
-	public int id;
-	public String end_time;
-	public String organizer_name;
-	public String name;
-	public String code_of_conduct;
-	public VersionModel _version;
-	public String topic;
-	public CreatorModel _creator;
-	public String email;
 	public String privacy;
+	public VersionModel _version;
+	public String state;
+	public String end_time;
+	public String location_name;
+	public String organizer_name;
+	public String organizer_description;
+	public ArrayList<Social_linksModel> social_links;
+	public CreatorModel _creator;
+	public Call_for_papersModel _call_for_papers;
+	public String topic;
+	public String description;
+	public String background_image;
+	public String code_of_conduct;
+	public String schedule_published_on;
+	public String start_time;
+	public String logo;
+	public String name;
+	public CopyrightModel _copyright;
+	public String email;
+	public int id;
 
-	public RootModel(String start_time, Call_for_papersModel call_for_papers, String timezone, String logo, String location_name, ArrayList<Social_linksModel> social_links, String organizer_description, String description, CopyrightModel copyright, String schedule_published_on, String type, String state, String background_image, int id, String end_time, String organizer_name, String name, String code_of_conduct, VersionModel version, String topic, CreatorModel creator, String email, String privacy) {
+	public RootModel(String timezone, String type, String privacy, VersionModel version, String state, String end_time, String location_name, String organizer_name, String organizer_description, ArrayList<Social_linksModel> social_links, CreatorModel creator, Call_for_papersModel call_for_papers, String topic, String description, String background_image, String code_of_conduct, String schedule_published_on, String start_time, String logo, String name, CopyrightModel copyright, String email, int id) {
 
-		this.start_time = start_time;
-		this._call_for_papers = call_for_papers;
 		this.timezone = timezone;
-		this.logo = logo;
-		this.location_name = location_name;
-		this.social_links = social_links;
-		this.organizer_description = organizer_description;
-		this.description = description;
-		this._copyright = copyright;
-		this.schedule_published_on = schedule_published_on;
 		this.type = type;
-		this.state = state;
-		this.background_image = background_image;
-		this.id = id;
-		this.end_time = end_time;
-		this.organizer_name = organizer_name;
-		this.name = name;
-		this.code_of_conduct = code_of_conduct;
-		this._version = version;
-		this.topic = topic;
-		this._creator = creator;
-		this.email = email;
 		this.privacy = privacy;
+		this._version = version;
+		this.state = state;
+		this.end_time = end_time;
+		this.location_name = location_name;
+		this.organizer_name = organizer_name;
+		this.organizer_description = organizer_description;
+		this.social_links = social_links;
+		this._creator = creator;
+		this._call_for_papers = call_for_papers;
+		this.topic = topic;
+		this.description = description;
+		this.background_image = background_image;
+		this.code_of_conduct = code_of_conduct;
+		this.schedule_published_on = schedule_published_on;
+		this.start_time = start_time;
+		this.logo = logo;
+		this.name = name;
+		this._copyright = copyright;
+		this.email = email;
+		this.id = id;
 
 	}
 
